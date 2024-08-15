7cb16bfdf376d4038c5f9e14fb2e3e8dd224273f
MamawliV2 <168989230+MamawliV2@users.noreply.github.com>
2024-08-15 22:32:50 +0330

    Update net.py

--- net.py
+++ net.py
@@ -1,22 +1,22 @@
 from telethon import TelegramClient, events
+import asyncio
 
-# تنظیمات ربات
-api_id = '1416439'  # api_id شما
-api_hash = '9ac5952633a05e37c246a9ce5b93b5b1'  # api_hash شما
-phone_number = '+989944308487'  # شماره تلفن شما
+# اطلاعات حساب کاربری تلگرام خود را وارد کنید
+api_id = '1416439'
+api_hash = '9ac5952633a05e37c246a9ce5b93b5b1'
+phone_number = '+989944308487'
 
-# ایجاد کلاینت تلگرام
+# ایجاد یک کلاینت تلگرام
 client = TelegramClient('session_name', api_id, api_hash)
 
-@client.on(events.NewMessage)
-async def handler(event):
-    # چاپ پیام‌های دریافتی
-    print(f"پیام جدید: {event.message.message}")
-
 async def main():
+    # اتصال به حساب کاربری
     await client.start(phone=phone_number)
-    print("ربات شروع به کار کرد")
-    await client.run_until_disconnected()
+    print("Client Created and Online")
 
-# اجرای ربات
+    # نگه داشتن کلاینت آنلاین
+    while True:
+        await asyncio.sleep(60)  # هر 60 ثانیه یکبار منتظر می‌ماند تا آنلاین بماند
+
+# اجرای برنامه
 client.loop.run_until_complete(main())
